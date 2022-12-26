031bcf7930ce25c7165953bc17c2e0b8c21abb1d
Stanley Waweru <stanleywaweru79@gmail.com>
2022-12-26 06:59:33 +0300

    First commit

---
+++ src/Categories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Categories = ({ filterItems, categories }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            className='filter-btn'
+            type='btn'
+            key={index}
+            onClick={() => {
+              filterItems(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
